4ce6144220db1e4e8ce059d85e8e55a9f74b6bfd
Siddish Sankar <siddishsankar@Siddishs-MacBook-Pro.local>
2022-04-09 22:56:20 -0700

    Removed special characters.

--- python/get-data.py
+++ python/get-data.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import json
-ingred = pd.read_csv('../Data/branded_food.csv')
-foods  = pd.read_csv('../Data/food.csv')
+ingred = pd.read_csv('../Data/originalbranded_food.csv')
+foods  = pd.read_csv('../Data/originalfood.csv')
 #data   = pd.concat([foods['fdc_id'], foods['description'], ingred['ingredients']], axis=1)
 data   = pd.concat([foods['description'], ingred['ingredients']], axis=1)
 data.drop_duplicates(subset='description', keep=False,inplace=True)
 data = data.dropna()
 foodList = data['description'].tolist()
+newfoodList = [food.replace('""', '"') for food in foodList]
 
-jsonfood = json.dumps(foodList)
+jsonfood = json.dumps(newfoodList)
 f = open("../js/suggestions.js", "w")
 f.write("let suggestions = " + jsonfood)
 f.close()
