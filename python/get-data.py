dbc750760d226d1e46ad4319751a968d47285654
SleepyDeep <aalam@ucsd.edu>
2022-04-09 20:07:18 -0700

    -working suggestions bar

--- python/get-data.py
+++ python/get-data.py
@@ -4,8 +4,12 @@ ingred = pd.read_csv('../Data/branded_food.csv')
 foods  = pd.read_csv('../Data/food.csv')
 #data   = pd.concat([foods['fdc_id'], foods['description'], ingred['ingredients']], axis=1)
 data   = pd.concat([foods['description'], ingred['ingredients']], axis=1)
-foodList = foods['description'].tolist()
-jsonFoodList = json.dumps(foodList)
-with open("./FoodSuggestions.json", "w") as outfile:
-    outfile.write(jsonFoodList)
-#print(data.head(5))
+data = data.dropna()
+foodList = data['description'].tolist()
+
+jsonfood = json.dumps(foodList)
+f = open("../js/suggestions.js", "w")
+f.write("let suggestions = " + jsonfood)
+f.close()
+
+#print(jsonfood[14014777:14014800])
